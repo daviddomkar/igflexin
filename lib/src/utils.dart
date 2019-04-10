b8841f3325619654cdfeb36a079df178d525b1ca
David Domkář <dave.domkar@gmail.com>
2019-04-11 00:58:12 +0200

    Intro animations

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/widgets.dart';
+
 class Utils {
-  static double computeResponsivity(double value, double devicePixelRatio) {
+  static double computeResponsivity(double value, BuildContext context) {
+
+    var queryData = MediaQuery.of(context);
+    double devicePixelRatio = queryData.devicePixelRatio;
+
     return devicePixelRatio < 2.625 ? value / (5.0 - devicePixelRatio) * (5.0 - 2.625) : value;
   }
 }
