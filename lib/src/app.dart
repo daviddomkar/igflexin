214037f987ea339ea06eb5ac689355d3e27aa579
David Domkář <dave.domkar@gmail.com>
2019-04-15 01:00:57 +0200

    Custom router

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Route;
 import 'package:flutter/services.dart';
+import 'package:igflexin/src/router.dart';
 
 import 'package:igflexin/src/ui/auth_ui.dart';
+import 'package:igflexin/src/utils.dart';
 
 class IGFlexinApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       systemNavigationBarColor: Colors.transparent,
@@ -19,7 +19,36 @@ class IGFlexinApp extends StatelessWidget {
         primaryColor: Color.fromARGB(255, 223, 61, 139),
         accentColor: Color.fromARGB(255, 255, 161, 94),
       ),
-      home: Auth(),
+      home: Router(
+        name: 'main',
+        routes: [
+          Route('auth', (context) {
+            return Auth();
+          }),
+          Route('app', (context) {
+            return Center(
+              child: RaisedButton(
+                highlightElevation: 0,
+                elevation: 0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50.0)),
+                color: Colors.white,
+                child: Text(
+                  'Klikni sem kámo',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: Utils.computeResponsivity(16.0, context),
+                    color: Color.fromARGB(255, 223, 61, 139),
+                  ),
+                ),
+                onPressed: () {
+                  RouterController.withName('main').switchRoute('auth');
+                },
+              ),
+            );
+          }),
+        ],
+        startingRoute: 'auth',
+      ),
     );
   }
 }
