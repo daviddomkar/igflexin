d974640bef58054e07b10fd381f3a1a35199d910
Filip Brebera <breberafilip@icloud.com>
2019-05-21 11:42:52 +0200

    Fixed scaling on iOS

--- lib/utils/responsivity_utils.dart
+++ lib/utils/responsivity_utils.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/widgets.dart';
 
+import 'dart:io' show Platform;
+
 class ResponsivityUtils {
   static double compute(double value, BuildContext context) {
     var queryData = MediaQuery.of(context);
     double devicePixelRatio = queryData.devicePixelRatio;
-
-    return devicePixelRatio < 2.625 ? value / (5.5 - devicePixelRatio) * (5.5 - 2.625) : value / devicePixelRatio * 2.625;
+    if (Platform.isAndroid) {
+      // Android-specific code
+      return devicePixelRatio < 2.625
+          ? value / (5.5 - devicePixelRatio) * (5.5 - 2.625)
+          : value / devicePixelRatio * 2.625;
+    } else if (Platform.isIOS) {
+      // iOS-specific code
+            return devicePixelRatio < 2.625
+          ? value / (14.0 - devicePixelRatio) * (14.0 - 2.625)
+          : value / devicePixelRatio * 2.625;
+    }
   }
 }
