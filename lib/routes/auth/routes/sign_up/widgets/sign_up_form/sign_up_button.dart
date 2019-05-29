013ba86678f0cf41c1e9b9b7934712adde8725bc
Filip Brebera <breberafilip@icloud.com>
2019-05-29 13:58:51 +0200

    Sign Up

--- lib/routes/auth/routes/sign_up/widgets/sign_up_form/sign_up_button.dart
+++ lib/routes/auth/routes/sign_up/widgets/sign_up_form/sign_up_button.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-import 'package:igflexin/utils/router_utils.dart';
 import 'package:igflexin/utils/responsivity_utils.dart';
 import 'package:igflexin/widgets/buttons.dart';
 
 class SignUpButton extends StatelessWidget {
-  SignUpButton({Key key, this.controller})
+  SignUpButton({Key key, this.controller, this.formKey})
       : scale = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
           parent: controller,
           curve: new Interval(0.400, 0.900, curve: Curves.elasticOut),
         )),
         super(key: key);
 
+  final GlobalKey<FormState> formKey;
   final AnimationController controller;
 
   final Animation<double> scale;
@@ -20,22 +20,21 @@ class SignUpButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Transform.scale(
       scale: scale.value,
-      origin: Offset(0.0, ResponsivityUtils.compute(25.0, context)),
       child: Container(
         width: ResponsivityUtils.compute(300.0, context),
         height: ResponsivityUtils.compute(50.0, context),
-        margin: EdgeInsets.only(top: ResponsivityUtils.compute(70.0, context)),
-        child: CurvedWhiteBorderedTransparentButton(
+        margin: EdgeInsets.only(top: ResponsivityUtils.compute(10.0, context)),
+        child: CurvedWhiteButton(
           child: Text(
-            "I am new to this app",
+            'Sign Up',
             textDirection: TextDirection.ltr,
             style: TextStyle(
               fontSize: ResponsivityUtils.compute(16.0, context),
-              color: Colors.white,
+              color: Color.fromARGB(255, 223, 61, 139),
             ),
           ),
           onPressed: () {
-            RouterController.of(context, 'auth').switchRoute('auth', 'signup');
+            formKey.currentState.validate();
           },
         ),
       ),
